ee590ceaa3f47fe77284544e99536b61810cc2a5
ZeeTW <zainab.alafoo@hotmail.com>
2024-11-25 22:48:44 +0300

    maybe done with 4? idk

--- server.js
+++ server.js
@@ -7,6 +7,16 @@ const collectibles = [
   { name: 'autographed picture of a dog', price: 10 },
   { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
 ]
+
+const shoes = [
+  { name: 'Birkenstocks', price: 50, type: 'sandal' },
+  { name: 'Air Jordans', price: 500, type: 'sneaker' },
+  { name: 'Air Mahomeses', price: 501, type: 'sneaker' },
+  { name: 'Utility Boots', price: 20, type: 'boot' },
+  { name: 'Velcro Sandals', price: 15, type: 'sandal' },
+  { name: 'Jet Boots', price: 1000, type: 'boot' },
+  { name: 'Fifty-Inch Heels', price: 175, type: 'heel' }
+]
 //routes
 
 //username
@@ -44,6 +54,21 @@ app.get('/collectibles/:index', (req, res) => {
       `<h1>So, you want the ${item.name}? For ${item.price}, it can be yours!</h1>`
     )
   }
+})
+
+//shoes
+app.get('/shoes', (req, res) => {
+  if (req.query['min-price']) {
+    shoes = shoes.filter((shoe) => shoe.price >= +req.query['min-price'])
+  }
+  if (req.query['max-price']) {
+    shoes = shoes.filter((shoe) => shoe.price >= +req.query['max-price'])
+  }
+  if (req.query.type) {
+    shoes = shoes.filter((shoe) => shoe.type === req.query.type)
+  }
+
+  res.send(shoes)
 })
 
 app.listen(3000, () => {
